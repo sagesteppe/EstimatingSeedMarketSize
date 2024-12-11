e869b21d3e74443fb07c5d7f3ec729b4782bc57b
sagesteppe <reedbenkendorf27@gmail.com>
2024-12-11 15:29:23 -0600

    some survey fleshed out

---
+++ survey/ReplicateSurveyQuestions.R
@@ -0,0 +1,5 @@
+### I don't want to write the survey questions by hand. Here we generate them using functions. 
+
+# We can generate each of the 12 slides using the same process. Basically copy and paste with some incrementing of numbers in the code. 
+
+
--- survey/app.R
+++ survey/app.R
@@ -25,10 +25,26 @@ db <- sd_database(
 server <- function(input, output, session) {
 
   # Define any conditional skip logic here (skip to page if a condition is true)
-  sd_skip_if()
+  sd_skip_if(
+   # input$respondent_DOI_region == 1 ~ "NAA_lifeform_area",
+  #  input$respondent_DOI_region == 2 ~ "SAG_lifeform_area",
+  #  input$respondent_DOI_region == 3 ~ "GL_lifeform_area",
+  #  input$respondent_DOI_region == 4 ~ "Mississippi_lifeform_area",
+  #  input$respondent_DOI_region == 5 ~ "Missouri_lifeform_area",
+  #  input$respondent_DOI_region == 6 ~ "ARGTG_lifeform_area",
+  #  input$respondent_DOI_region == 7 ~ "UCB_lifeform_area",
+  #  input$respondent_DOI_region == 8 ~ "LCB_lifeform_area",
+  #  input$respondent_DOI_region == 9 ~ "CPN_lifeform_area",
+  #  input$respondent_DOI_region == 10 ~ "CGB_lifeform_area",
+    input$respondent_DOI_region == 11 ~ "Alaska_lifeform_area"#,
+  #  input$respondent_DOI_region == 12 ~ "Pacific_lifeform_area"
+  )
 
   # Define any conditional display logic here (show a question if a condition is true)
-  sd_show_if()
+  sd_show_if(
+    input$respondentAgency == "other" ~ "respondentAgency_other", 
+    input$email_followup == 'Yes' ~ "respondent_email_address"
+  )
 
   # Database designation and other settings
   sd_server(
