3417b2dcf0c87338f21109af5f8c0191f8d71810
sagesteppe <reedbenkendorf27@gmail.com>
2025-02-24 12:47:33 -0600

    working towards fn for plotting quantile regression

--- scripts/QuantileRegression.R
+++ scripts/QuantileRegression.R
@@ -3,50 +3,67 @@ setwd('~/Documents/assoRted/EstimatingSeedMarketSize/scripts')
 library(tidyverse)
 library(quantreg)
 
-firesYear <- read.csv(file.path('..', 'data', 'processed', 
+x <- read.csv(file.path('..', 'data', 'processed', 
                                 'NoFires-TotalArea_byDOIRegion.csv')) |>
   filter(REG_NAME == 'California-Great Basin') 
 
-# note we can feed wts into this function, the most simple weights that we can try 
-# and impose would be a linear decay, although I wonder whether an exponential or
-# other type of decay would be admissible
-wts = rep(1, length.out = nrow(firesYear))
-wts <- seq(0.1, 1, length.out = nrow(firesYear)) # linear decay
-wts <- exp(wts)
+resp <- 'TotalArea_Acre'
+pred <- 'FIRE_YEAR'
+wts_p <- 'linear'
+quants <- c(0.025, 0.4, 0.5, 0.6, 0.75, 0.95)
+form <- as.formula(paste0(resp, '~', pred))
 
-# plot(seq(0.01, 1, length.out = length(wts)), wts) # what we want, although more decay in the tail wouldn't be bad
+# wts can be feed into the function if more recent values are more important than
+# historic values. 
 
-smodel.lm <- lm(TotalArea_Acre ~ FIRE_YEAR, data = firesYear)
-model.lwr <- rq(TotalArea_Acre ~ FIRE_YEAR, data = firesYear, tau = 0.025, weights = wts)
-model.25 <- rq(TotalArea_Acre ~ FIRE_YEAR, data = firesYear, tau = 0.25, weights = wts)
-model.4 <- rq(TotalArea_Acre ~ FIRE_YEAR, data = firesYear, tau = 0.4, weights = wts)
-model.5 <- rq(TotalArea_Acre ~ FIRE_YEAR, data = firesYear, tau = 0.5, weights = wts)
-model.6 <- rq(TotalArea_Acre ~ FIRE_YEAR, data = firesYear, tau = 0.6, weights = wts)
-model.75 <- rq(TotalArea_Acre ~ FIRE_YEAR, data = firesYear, tau = 0.75, weights = wts)
-model.upr <- rq(TotalArea_Acre ~ FIRE_YEAR, data = firesYear, tau = 0.975, weights = wts)
+if(wts_p=='none'){
+  wts <- rep(1, length.out = nrow(x))
+} else {
+  wts <- seq(0.1, 1, length.out = nrow(x))
+}
+if(wts_p=='exp'){
+  wts <- exp(wts)
+}
+
+quantL <- vector(mode = 'list', length = length(quants))
+names(quantL) <- paste0('model.', quants)
+
+for (i in seq(quants)){ 
+  quantL[[i]] <- rq(form, data = x, tau = wts[i], weights = wts)
+}
+
+model.lm <- lm(form, data = x)
 
 # I would expect this to force quantiles from 0.01 to 0.99 to be calculated at 
 # intervals of 0.01 based on the documentation. I'm not really sure why that 
 # isn't what is happening (per the documentation). 
-all <- rq(TotalArea_Acre ~ FIRE_YEAR, data = firesYear, tau = 1.01)
+
+linetype <- c()
+cols <- paste0('grey', seq(0, 100, length.out = 4))
 
 par(mar = c(7, 5, 4, 2))
 plot(
-  x = firesYear$FIRE_YEAR,
-  y = firesYear$TotalArea_Acre,
-  main = paste0('Total Area Burned - quantile regression\n', firesYear[['REG_NAME']][1]),
+  x = x[[pred]],
+  y = x[[resp]],
+  main = paste0('Total Area Burned - quantile regression\n', x[['REG_NAME']][1]),
   xlab = 'Fire Year', 
   ylab = 'Total Area (Acre)',
   pch = 20,
   cex = 1.2,
   yaxt = "n"
 ) 
+abline(model.lm, col = 'blue')
+
+
+for (i in seq()){
+  abline(quantL[[i]], lty = linetype[i], col = cols[i])
+}
 abline(model.5, col = 'grey0')
 abline(model.4, lty = 5, col = 'grey20')
 abline(model.6, lty = 5, col = 'grey20')
 abline(model.25, lty = 4, col = 'grey40')
 abline(model.75, lty = 4, col = 'grey40')
-abline(model.lwr, lty = 3, col = 'grey60')
+abline(model.lwr, lty = 3, col = 'grey80')
 abline(model.upr, lty = 3, col = 'grey60')
 axis(2, 
      at = labs <- pretty(par()$usr[3:4]),
@@ -56,9 +73,25 @@ axis(2,
 
 legend(
   x = "topleft", 
-  legend = c("Median (0.5", "0.4 & 0.6", 'quartiles (0.25,0.75)', '0.95 band (0.025,0.975)'),
+  legend = c("Median (0.5", "0.4 & 0.6", 'Quartiles (0.25,0.75)', '0.95 band (0.025,0.975)'),
   lty = c(1, 5:3),  
   col = paste0('grey', seq(0, 60, length.out = 4)),
   lwd = 2, 
   bg = adjustcolor("white", 0.4)
 )  
+
+
+#' Fit quantile regression models to a time series using an autoregressive process. 
+#' 
+#' @description 
+#' @param x Data frame. Must contain all data required to perform modelling. 
+#' @param quants Numeric vector - odd. The quantiles which you want to model fits for, defaults to c(0.025, 0.25, 0.5, 0.75, 0.95), for the 0.95 band, the quartiles, and the median. 
+#' @param wts_p Character. One of 'none' for unweighted regression (the default), 'linear' for a linear decay of weights, or 'exp' for exponential decay of weigts. 
+#' @param resp Character. Name of the response field.  
+#' @param pred Character. Name of the predictor field.  
+#' @param plotLM Boolean. Defaults to TRUE, whether to also plot the fit of a linear model to the plot. 
+quartReg <- function(x){
+  
+  
+  
+}
